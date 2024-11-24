bc8affe5b7405bf8cd2b9fc3eb1280567837b9fc
borodkoleg <borodkoleg@gmail.com>
2024-11-24 19:58:35 +0200

    Link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      Home Page
+      <h1>Home Page</h1>
+      <Link href="/login">Login</Link>
     </div>
   );
 }
